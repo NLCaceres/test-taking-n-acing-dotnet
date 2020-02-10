5711f481127bdcb0a0e40422355d27715a38769c
NLCaceres <NLCaceres21@Gmail.com>
2020-02-10 13:12:30 -0800

    Swap In memory DB for cloud atlas mongoDB

--- Server/Startup.cs
+++ Server/Startup.cs
@@ -1,23 +1,39 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.ResponseCompression;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Options;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using TestTaking.Server.Models;
+using TestTaking.Server.Services;
 
 namespace TestTaking.Server
 {
   public class Startup
   {
+    public Startup(IConfiguration configuration)
+    {
+      Configuration = configuration;
+    }
+    public IConfiguration Configuration { get; }
+
     // This method gets called by the runtime. Use this method to add services to the container.
     // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
-      services.AddDbContext<PracticeTestContext>(opt =>
-      opt.UseInMemoryDatabase("PracticeTestList")); // In-Memory DBs live in server RAM, quick access since no need to pull it up from the disk. 
+
+      services.Configure<PracticeTestsDatabaseSettings>(
+                Configuration.GetSection(nameof(PracticeTestsDatabaseSettings)));
+
+      services.AddSingleton<IPracticeTestsDatabaseSettings>(sp =>
+          sp.GetRequiredService<IOptions<PracticeTestsDatabaseSettings>>().Value);
+      services.AddSingleton<PracticeTestService>(); // Singleton because MongoClient used to grab from DB requires Singleton service lifetime (direct dependency)
+
+      // services.AddDbContext<PracticeTestContext>(opt => opt.UseInMemoryDatabase("PracticeTestList")); // In-Memory DBs live in server RAM, quick access since no need to pull it up from the disk. 
       services.AddResponseCompression(opts =>
       {
         opts.MimeTypes = ResponseCompressionDefaults.MimeTypes.Concat(
